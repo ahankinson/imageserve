eeaddf8f51bdbc2e6dfdfab095e6e6f5a37bd5e4
Jamie Klassen <michigan.j.frog@gmail.com>
2012-11-06 10:56:23 -0500

    Fixed a few small problems
    It doesn't quite run yet, still need to adjust a few things.

--- imageserve/settings.py
+++ imageserve/settings.py
@@ -107,7 +107,7 @@ ROOT_URLCONF = 'imageserve.urls'
 WSGI_APPLICATION = 'imageserve.wsgi.application'
 
 TEMPLATE_DIRS = (
-    '/Users/jamieklassen/Documents/Code/imageserve/imageserve/imageserveapp',
+    '/Users/jamieklassen/Documents/Code/imageserve/imageserveapp',
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
