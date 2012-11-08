7d5972b642acd6636e91b0ef95253c4adf6d999d
Jamie Klassen <michigan.j.frog@gmail.com>
2012-11-08 10:50:10 -0500

    Now it works
    For me, I guess I had some problems with using memcached, running nginx and
    running iipsrv from the command line. What's weird is that strictly speaking
    it doesn't work, it just doesn't crash. I can view the images individually
    by calling iipsrv.fcgi myself, but in the diva interface they just "load"
    endlessly.

--- imageserveapp/conf.py
+++ imageserveapp/conf.py
@@ -1,6 +1,6 @@
 
 IMG_DIR = "/Users/jamieklassen/Documents/Code/imageserve/images/"
 APP_ROOT = "/"
-MEMCACHED_ENABLED = True
+MEMCACHED_ENABLED = False
 MEMCACHED_SERVER = "127.0.0.1:11211"
 TMP_DIR = "/tmp/diva.js"
