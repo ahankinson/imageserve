c68b7dc6844efb5b038f2ef0fb730d824397c5ce
Jamie Klassen <michigan.j.frog@gmail.com>
2012-11-27 14:33:02 -0500

    Add ManuscriptGroup to admin site

--- imageserveapp/admin.py
+++ imageserveapp/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from imageserveapp.models import Manuscript
+from imageserveapp.models import Manuscript, ManuscriptGroup
 
 admin.site.register(Manuscript)
+admin.site.register(ManuscriptGroup)
