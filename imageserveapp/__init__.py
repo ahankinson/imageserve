c88bed252c0091a2546f4897d37e77b1bfec6fcb
Jamie Klassen <michigan.j.frog@gmail.com>
2012-11-27 14:32:31 -0500

    shortcut method for ISMI definitions

--- imageserveapp/__init__.py
+++ imageserveapp/__init__.py
@@ -15,3 +15,19 @@ def get_by_ismi_id(iden):
     ent = loads(u.read())['ent']
     u.close()
     return ent
+
+def ismi_witness_def():
+    """
+    Get the definition of the WITNESS object class in the ISMI database.
+    This definition is serialized as a JSON object, but this method
+    returns a Python dict, which contains descriptions of all the
+    fields, attributes and relations which the ISMI database permits on
+    a WITNESS object.
+    """
+    u = urlopen(JSON_INTERFACE+"method=get_defs")
+    defs = loads(u.read())['defs']
+    u.close()
+    w = [d for d in defs if d.get('ov') == 'WITNESS'][0]
+    return w
+
+	
