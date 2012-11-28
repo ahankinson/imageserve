2d8877119a716ecd454a71bf67096074d6e6dca3
Jamie Klassen <michigan.j.frog@gmail.com>
2012-11-28 17:42:24 -0500

    Changed views
    Now the settings are used, though it is very slow and not
    completely working. The first step in Ich's programming
    philosophy: make it work (though not "right").

--- imageserveapp/views.py
+++ imageserveapp/views.py
@@ -6,7 +6,7 @@ from django.shortcuts import render
 from django.utils.encoding import smart_text
 from divaserve import tryint, alphanum_key
 from imageserveapp import img_server, JSON_INTERFACE, get_by_ismi_id
-from imageserveapp.models import Manuscript
+from imageserveapp.models import Manuscript, AttDisplaySetting, RelDisplaySetting
 
 def main(request):
     """The main view, where users can browse available manuscripts."""
@@ -26,7 +26,9 @@ def canvas(request):
 def manuscript(request, ms_id):
     """The view for displaying a specific manuscript."""
     m = Manuscript.objects.get(id=ms_id)
-    ent = get_by_ismi_id(m.ismi_id)
+    entity = get_by_ismi_id(m.ismi_id)
+    rl = [r for r in entity['src_rels'] if r['name'] == 'is_exemplar_of'][0]
+    text = get_by_ismi_id(rl['tar_id'])
     pth = os.path.join(conf.IMG_DIR, m.directory)
     def htmlify(s):
         """
@@ -46,22 +48,38 @@ def manuscript(request, ms_id):
         else:
             post += unicode(s)
         return post
-    ms_title = m.title()
-    ms_author = m.author()
+    def get_keyval(setting):
+        att_name = setting.name
+        key = setting.display_name
+        ent = entity
+        val = ''
+        if not setting.on_ent:
+            ent = text
+        if isinstance(setting, RelDisplaySetting):
+            hits = [r for r in ent[setting.rel_type+"_rels"]
+                    if r['name'] == att_name]
+            if hits:
+                rel = hits[0]
+                opp = 'tar' if setting.rel_type == 'src' else 'src'
+                ent = get_by_ismi_id(rel[opp+"_id"])
+                att_name = 'ov'
+                if setting.show_id:
+                    val = " (ID {0:n})".format(ent['id'])
+        if ent.get(att_name):
+            val = ent.get(att_name) + val
+        else:
+            hits = [a for a in ent['atts'] if a['name'] == att_name]
+            if hits:
+                if hits[0].get('ov'):
+                    val = hits[0].get('ov') + val
+        if not val:
+            val = 'Data missing'
+        return (key, val)
+    ms_title = get_keyval(RelDisplaySetting.objects.get(name='is_exemplar_of'))[1]
+    ms_author = get_keyval(RelDisplaySetting.objects.get(name='was_created_by'))[1]
     title = u'{0}<br /><br />Author: {1}'.format(ms_title, ms_author)
-    md = {
-        'Title': ms_title,
-        'Author': ms_author,
-        'Attributes': {
-            att['name']: att.get('ov') for att in ent['atts']
-        },
-        'Source Relations': {
-            rel['name']: get_by_ismi_id(rel['tar_id']).get('ov') for rel in ent['src_rels']
-        },
-        'Target Relations': {
-            rel['name']: get_by_ismi_id(rel['src_id']).get('ov') for rel in ent['tar_rels']
-        },
-    }
+    md = dict([get_keyval(a) for a in AttDisplaySetting.objects.filter(show=True)]+
+              [get_keyval(r) for r in RelDisplaySetting.objects.filter(show=True)])
     data = {
         'md': htmlify(md),
         'image_path': pth,
