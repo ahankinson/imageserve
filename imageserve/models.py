2201a68c37fc10bdb69f72903d5a8c61e87d5511
Jamie Klassen <michigan.j.frog@gmail.com>
2013-01-22 18:42:01 -0500

    beginning "witnesses-in-codex" implementation

--- imageserve/models.py
+++ imageserve/models.py
@@ -1,11 +1,73 @@
 from os import listdir
 from os.path import isdir, join
 from django.db import models
+from django.forms.fields import MultiValueField, IntegerField
+from django.forms import MultiWidget
+from django.forms.util import ValidationError as FormValidationError
 from conf import IMG_DIR
 from imageserve.helpers import get_by_ismi_id
 from south.modelsinspector import add_introspection_rules
 
 add_introspection_rules([],["^imageserve\.models\.FolderField"])
+add_introspection_rules([],["^imageserve\.models\.WitnessListField"])
+
+class WitnessListWidget(MultiWidget):
+	def __init__(self, *args, **kwargs):
+		fields = kwargs['fields']
+		self.labels = [f.label for f in fields]
+		data = {'widgets': (f.widget for f in fields)}
+		super(WitnessListWidget, self).__init__(*args, **data)
+	def decompress(self, value):
+		print 'decompress called', value
+		if value:
+			return [i for _,i in eval(value)]
+		return [None for w in self.labels]
+	def format_output(self, rendered_widgets):
+		z = zip(self.labels,rendered_widgets)
+		rows = u'\n'.join(u'<tr><td>'+l+u': '+w+u'</td></tr>' for l,w in z)
+		return u'<table>' + rows + u'</table>'
+
+class WitnessListFormField(MultiValueField):
+	def __init__(self, *args, **kwargs):
+		self.witnesses = kwargs['witnesses']
+		fields = tuple(IntegerField(
+			label=get_by_ismi_id(w).get('ov')
+		) for w in self.witnesses)
+		data = {
+			'fields': fields,
+			'widget': WitnessListWidget(fields=fields),
+			'label': 'Witnesses in this codex, with start pages'
+		}
+		super(WitnessListFormField, self).__init__(*args, **data)
+	def compress(self, data_list):
+		return zip(self.witnesses, data_list)
+
+class WitnessListField(models.Field):
+	description = """A list of the witnesses contained in a codex,
+	along with the page in the codex they begin on"""
+	__metaclass__ = models.SubfieldBase
+	def __init__(self, *args, **kwargs):
+		self.witnesses = kwargs.get('witnesses')
+		super(WitnessListField, self).__init__(*args, **kwargs)
+	def db_type(self, connection):
+		return 'char(500)'
+	def to_python(self, value):
+		if isinstance(value, list):
+			return value
+		elif isinstance(value, basestring):
+			return eval(value)
+		else:
+			return None
+	def get_db_prep_value(self, value, connection, prepared):
+		print 'get_db_prep_value called', value
+		return str(value)
+	def value_to_string(self, instance):
+		value = self._get_val_from_obj(instance)
+		return self.get_db_prep_value(value)
+	def formfield(self, form_class=WitnessListFormField, **kwargs):
+		defaults = {"witnesses": self.witnesses}
+		defaults.update(kwargs)
+		return form_class(**defaults)
 
 class FolderField(models.FilePathField):
 	"""
@@ -133,14 +195,23 @@ class RelDisplaySetting(models.Model):
 
 class Manuscript(models.Model):
 	"""
-	The model for all manuscripts in the RASI database.
+	The model for all manuscripts in the RASI database. These correspond
+	to Codex objects in the ISMI database for metadata purposes.
 	"""
 	directory = FolderField(path=IMG_DIR)
 	ismi_id = models.IntegerField(blank=True, null=True)
 	num_files = models.IntegerField(editable=False)
+	witnesses = WitnessListField(editable=False)
 	
 	def save(self, *args, **kwargs):
 		self.num_files = len(listdir(join(IMG_DIR, self.directory)))
+		if self.witnesses is None:
+			c = get_by_ismi_id(self.ismi_id)
+			wits = [r['src_id'] for r in c['tar_rels'] if r['name'] == 'is_part_of']
+			w = self._meta.get_field_by_name('witnesses')[0]
+			w.witnesses = wits
+			w.editable = True
+			self.witnesses = str([(w,None) for w in wits])
 		super(Manuscript, self).save(*args, **kwargs)
 	
 	def __unicode__(self):
