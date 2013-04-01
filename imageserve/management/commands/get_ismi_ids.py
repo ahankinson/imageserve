98e60d5378ab1b86decf59d3c875bd2f8cba3504
Jamie Klassen <michigan.j.frog@gmail.com>
2013-04-01 15:13:29 -0400

    added django-admin command "get_ismi_ids"...
    which you can use with `python manage.py get_ismi_ids` to automatically
    add ISMI ids for the stabi codices (well, the ones that are in ISMI, it seems).

---
+++ imageserve/management/commands/get_ismi_ids.py
@@ -0,0 +1,25 @@
+from django.core.management.base import BaseCommand
+from imageserve.models import Manuscript
+from csv import reader
+import os
+
+class Command(BaseCommand):
+    help = 'Updates as many ISMI IDs for Stabi codices as possible'
+
+    def handle(self, *args, **options):
+        path = os.path.realpath(
+            os.path.join(os.getcwd(), os.path.dirname(__file__))
+        )
+        fp = open(path+'/ismi_ids.csv','r')
+        rdr = reader(fp)
+        for directory, ismi_id in list(rdr)[1:]:
+            print 'getting ISMI ID for', directory, '...',
+            ms = Manuscript.objects.filter(directory=directory)
+            if ms:
+                ms = Manuscript.objects.get(directory=directory)
+                ms.ismi_id = ismi_id
+                ms.clean()
+                ms.save()
+                print 'done.'
+            else:
+                print directory, 'not found.'
