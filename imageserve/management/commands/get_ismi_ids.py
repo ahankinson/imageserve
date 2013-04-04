5ec783357591474deb59df93900fa73d6dc071c8
Jamie Klassen <michigan.j.frog@gmail.com>
2013-04-04 10:54:11 -0400

    more auto improvements

--- imageserve/management/commands/get_ismi_ids.py
+++ imageserve/management/commands/get_ismi_ids.py
@@ -1,18 +1,27 @@
 from django.core.management.base import BaseCommand
 from imageserve.models import Manuscript
-from csv import reader
+from imageserve.settings import JSON_INTERFACE
+from urllib import urlopen
+from json import loads
+from stabi_codices import STABI_CODICES
 import os
 
 class Command(BaseCommand):
     help = 'Updates as many ISMI IDs for Stabi codices as possible'
 
     def handle(self, *args, **options):
-        path = os.path.realpath(
-            os.path.join(os.getcwd(), os.path.dirname(__file__))
-        )
-        fp = open(path+'/ismi_ids.csv','r')
-        rdr = reader(fp)
-        for directory, ismi_id in list(rdr)[1:]:
+        u = urlopen(JSON_INTERFACE+"method=get_ents&oc=CODEX")
+        ents = loads(u.read())['ents']
+        u.close()
+        pairs = []
+        for ent in ents:
+            if ent.get('ov'):
+                ent['ov'] = ent['ov'].replace('.','').replace(' ','_')
+        for directory in STABI_CODICES:
+            matches = [e for e in ents if e.get('ov') == directory]
+            if matches:
+                zipper.append((directory, matches[0]['id']))
+        for directory, ismi_id in pairs:
             print 'getting ISMI ID for', directory, '...',
             ms = Manuscript.objects.filter(directory=directory)
             if ms:
