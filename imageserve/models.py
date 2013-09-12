6c9a4dc210231933a01eb7f83e201352e0b56d6d
Andrew Hankinson <andrew.hankinson@gmail.com>
2013-09-12 11:17:08 -0400

    Remove ISMI id validation
    See if this speeds things up.

--- imageserve/models.py
+++ imageserve/models.py
@@ -34,12 +34,12 @@ class IsmiIdField(models.IntegerField):
     Codex object from the ISMI database.
     """
     def to_python(self, value):
-        try:
-            c = get_by_ismi_id(value)
-        except KeyError:
-            raise ValidationError("Invalid ISMI ID")
-        if c['oc'] != 'CODEX':
-            raise ValidationError("ISMI entity must be of type CODEX")
+        # try:
+        #     c = get_by_ismi_id(value)
+        # except KeyError:
+        #     raise ValidationError("Invalid ISMI ID")
+        # if c['oc'] != 'CODEX':
+        #     raise ValidationError("ISMI entity must be of type CODEX")
         return value
 
 
