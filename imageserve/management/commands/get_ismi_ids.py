ec601e1ae8e305d25be33e43b73329a061018b92
Andrew Hankinson <andrew.hankinson@gmail.com>
2013-09-16 13:50:15 -0400

    Update get_ismi_ids management script

--- imageserve/management/commands/get_ismi_ids.py
+++ imageserve/management/commands/get_ismi_ids.py
@@ -26,33 +26,3 @@ class Command(BaseCommand):
                     ms.update(ismi_id=entity_id)
                 else:
                     print("Entry for {0} not found.".format(entity_name))
-
-
-
-    # def handle(self, *args, **options):
-    #     u = requests.get("{0}method=get_ents&oc=CODEX".format(settings.JSON_INTERFACE))
-    #     ents = json.loads(u.read())['ents']
-    #     u.close()
-    #     pairs = []
-    #     for ent in ents:
-    #         if ent.get('ov'):
-    #             ent['ov'] = ent['ov'].replace('.','').replace(' ','_')
-    #     for directory in STABI_CODICES:
-    #         matches = [e for e in ents if e.get('ov') == directory]
-    #         if matches:
-    #             pairs.append((directory, matches[0]['id']))
-    #     for directory, ismi_id in pairs:
-    #         print("getting ISMI ID for {0} ...".format(directory))
-    #         ms = Manuscript.objects.filter(directory=directory)
-    #         if ms:
-    #             try:
-    #                 ms = Manuscript.objects.get(directory=directory)
-    #                 ms.ismi_id = ismi_id
-    #                 ms.clean()
-    #                 ms.save()
-    #                 print('done.')
-    #             except ValidationError:
-    #                 print ('{0} failed.'.format(directory))
-    #                 continue
-    #         else:
-    #             print('{0} not found.'.format(directory))
