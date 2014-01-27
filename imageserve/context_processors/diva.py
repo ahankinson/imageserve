e0746f917eeac0ce590191d538e42cd0baa27a60
Andrew Hankinson <andrew.hankinson@gmail.com>
2014-01-27 11:57:10 -0500

    Fixed: Updated context processor for new settings

--- imageserve/context_processors/diva.py
+++ imageserve/context_processors/diva.py
@@ -2,6 +2,6 @@ from django.conf import settings
 
 
 def diva_settings(request):
-    return {'DIVASERVE_URL': settings.DIVASERVE_URL,
+    return {'OBJECT_DATA': settings.OBJECT_DATA,
             'IIPSERVER_URL': settings.IIPSERVER_URL,
             'IMG_DIR': settings.IMG_DIR}
